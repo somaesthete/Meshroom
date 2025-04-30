bf299188f3f39f2e8029b7a2e52dadb4791017e3
Nathan <nargetdev@gmail.com>
2025-04-29 21:58:45 -0700

    update qt link

--- docker/build-ubuntu.sh
+++ docker/build-ubuntu.sh
@@ -14,7 +14,7 @@ test -d docker || (
 test -d dl || \
 	mkdir dl
 test -f dl/qt.run || \
-        wget --no-check-certificate "https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run" -O "dl/qt.run"
+        wget --no-check-certificate "https://download.qt.io/official_releases/online_installers/4.7/qt-unified-linux-x64-4.7.0-online.run" -O "dl/qt.run"
 
 # Download a prebuilt assimp importer to address https://bugreports.qt.io/browse/QTBUG-88821
 test -f dl/libassimpsceneimport.so || \
